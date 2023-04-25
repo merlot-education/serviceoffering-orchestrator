f2cb7d010ca59c6dc01b95d9e78e589aebdf873c
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-04-25 10:16:47 +0200

    initial commit

---
+++ src/main/java/eu/merloteducation/serviceofferingorchestrator/ServiceofferingOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package eu.merloteducation.serviceofferingorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceofferingOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceofferingOrchestratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/merloteducation/serviceofferingorchestrator/ServiceofferingOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.merloteducation.serviceofferingorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceofferingOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
