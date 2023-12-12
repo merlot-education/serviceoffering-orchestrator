de3a9218b5c97c6a55d9d9a81bf8f147bbeaf490
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-12-12 17:19:48 +0100

    use merlot cert

--- src/main/java/eu/merloteducation/serviceofferingorchestrator/service/GXFSSignerService.java
+++ src/main/java/eu/merloteducation/serviceofferingorchestrator/service/GXFSSignerService.java
@@ -11,9 +11,7 @@ import foundation.identity.jsonld.JsonLDObject;
 import info.weboftrust.ldsignatures.LdProof;
 import info.weboftrust.ldsignatures.jsonld.LDSecurityKeywords;
 import info.weboftrust.ldsignatures.signer.JsonWebSignature2020LdSigner;
-import info.weboftrust.ldsignatures.signer.LdSigner;
 import info.weboftrust.ldsignatures.verifier.JsonWebSignature2020LdVerifier;
-import info.weboftrust.ldsignatures.verifier.LdVerifier;
 import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openssl.PEMParser;
@@ -23,17 +21,43 @@ import java.io.*;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.security.*;
+import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.security.interfaces.RSAPublicKey;
 import java.util.Date;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GXFSSignerService {
 
+    private final Logger logger = LoggerFactory.getLogger(GXFSSignerService.class);
+    private final PrivateKey prk;
+    private final List<X509Certificate> certs;
+
+    public GXFSSignerService(@Value("${gxfscatalog.cert-path}") String certPath,
+                             @Value("${gxfscatalog.private-key-path}") String privateKeyPath) throws IOException, CertificateException {
+        try (InputStream privateKeyStream = new FileInputStream(privateKeyPath)) {
+            PEMParser pemParser = new PEMParser(new InputStreamReader(privateKeyStream));
+            JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
+            PrivateKeyInfo privateKeyInfo = PrivateKeyInfo.getInstance(pemParser.readObject());
+
+            prk = converter.getPrivateKey(privateKeyInfo);
+        }
+
+        //---extract Expiration Date--- https://stackoverflow.com/a/11621488
+        try (InputStream publicKeyStream = new FileInputStream(certPath)) {
+            String certString = new String(publicKeyStream.readAllBytes(), StandardCharsets.UTF_8);
+            ByteArrayInputStream certStream = new ByteArrayInputStream(certString.getBytes(StandardCharsets.UTF_8));
+            CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+            certs = (List<X509Certificate>) certFactory.generateCertificates(certStream);
+        }
+    }
 
     /**
      * Given a verifiable presentation, sign and return it.
@@ -49,45 +73,35 @@ public class GXFSSignerService {
         VerifiablePresentation vp = VerifiablePresentation.fromJson(verifiablePresentationJson);
         VerifiableCredential vc = vp.getVerifiableCredential();
 
-        System.out.println("Signing VC");
-        LdProof vc_proof = sign(vc);
-        check(vc, vc_proof);
-        System.out.println("Signed");
+        logger.debug("Signing VC");
+        LdProof vcProof = sign(vc);
+        check(vc, vcProof);
+        logger.debug("Signed");
 
         vc.setJsonObjectKeyValue("proof", vc.getLdProof().getJsonObject());
         vp.setJsonObjectKeyValue("verifiableCredential", vc.getJsonObject());
 
-        System.out.println("Signing VC");
-        LdProof vp_proof = sign(vp);
-        check(vp, vp_proof);
-        System.out.println("Signed");
+        logger.debug("Signing VP");
+        LdProof vpProof = sign(vp);
+        check(vp, vpProof);
+        logger.debug("Signed");
 
         vp.setJsonObjectKeyValue("proof", vp.getLdProof().getJsonObject());
 
         return vp.toString();
     }
 
-    private static LdProof sign(JsonLDObject credential) throws IOException, GeneralSecurityException, JsonLDException {
-        InputStream privateKeyStream = GXFSSignerService.class.getClassLoader().getResourceAsStream("prk.ss.pem");
-
-        PEMParser pemParser = new PEMParser(new InputStreamReader(privateKeyStream));
-        JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
-        PrivateKeyInfo privateKeyInfo = PrivateKeyInfo.getInstance(pemParser.readObject());
-
-        PrivateKey prk = converter.getPrivateKey(privateKeyInfo);
-
+    private LdProof sign(JsonLDObject credential) throws IOException, GeneralSecurityException, JsonLDException {
         KeyPair kp = new KeyPair(null, prk);
         PrivateKeySigner<?> privateKeySigner = new RSA_PS256_PrivateKeySigner(kp);
 
-        LdSigner signer = new JsonWebSignature2020LdSigner(privateKeySigner);
+        JsonWebSignature2020LdSigner signer = new JsonWebSignature2020LdSigner(privateKeySigner);
 
         signer.setCreated(new Date());
         signer.setProofPurpose(LDSecurityKeywords.JSONLD_TERM_ASSERTIONMETHOD);
-        signer.setVerificationMethod(URI.create("did:web:compliance.lab.gaia-x.eu"));
+        signer.setVerificationMethod(URI.create("did:web:merlot-education.eu"));
 
-        LdProof ldProof = signer.sign(credential);
-
-        return ldProof;
+        return signer.sign(credential);
     }
 
     /**
@@ -99,23 +113,12 @@ public class GXFSSignerService {
      * @throws GeneralSecurityException GeneralSecurityException
      * @throws JsonLDException          JsonLDException
      */
-    public static void check(JsonLDObject credential, LdProof proof) throws IOException, GeneralSecurityException, JsonLDException {
-        //---extract Expiration Date--- https://stackoverflow.com/a/11621488
-        InputStream publicKeyStream = GXFSSignerService.class.getClassLoader().getResourceAsStream("cert.ss.pem");
-        String certString = new String(publicKeyStream.readAllBytes(), StandardCharsets.UTF_8);
-        ByteArrayInputStream certStream = new ByteArrayInputStream(certString.getBytes(StandardCharsets.UTF_8));
-        CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
-        List<X509Certificate> certs = (List<X509Certificate>) certFactory.generateCertificates(certStream);
-
+    private void check(JsonLDObject credential, LdProof proof) throws IOException, GeneralSecurityException, JsonLDException {
         for (X509Certificate cert : certs) {
-
             PublicKey puk = cert.getPublicKey();
             PublicKeyVerifier<?> pkVerifier = new RSA_PS256_PublicKeyVerifier((RSAPublicKey) puk);
-
-            LdVerifier verifier = new JsonWebSignature2020LdVerifier(pkVerifier);
-
-            System.out.println(verifier.verify(credential, proof));
+            JsonWebSignature2020LdVerifier verifier = new JsonWebSignature2020LdVerifier(pkVerifier);
+            verifier.verify(credential, proof);
         }
     }
-
 }
