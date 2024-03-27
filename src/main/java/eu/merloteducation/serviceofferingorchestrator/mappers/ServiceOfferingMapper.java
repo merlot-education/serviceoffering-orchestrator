b7129a15726a679ae374074def6d1202c81a5f2c
Kim Chi Tran <146179578+kimchitran-cap@users.noreply.github.com>
2024-03-27 13:42:31 +0100

    Revert "Add catalog call to retrieve service offering sd signer legal name"
    This reverts commit c06b12ba1c3d44ee6fef8b538a01c7b285c4a264.

--- src/main/java/eu/merloteducation/serviceofferingorchestrator/mappers/ServiceOfferingMapper.java
+++ src/main/java/eu/merloteducation/serviceofferingorchestrator/mappers/ServiceOfferingMapper.java
@@ -46,11 +46,9 @@ public interface ServiceOfferingMapper {
     @Mapping(target = "metadata.hash", source = "extension.currentSdHash")
     @Mapping(target = "metadata.creationDate", source = "extension.creationDate")
     @Mapping(target = "metadata.modifiedDate", source = "selfDescriptionMeta.statusDatetime")
-    @Mapping(target = "metadata.signedBy", source = "signerLegalName")
     @Mapping(target = "selfDescription", source = "selfDescriptionMeta.content")
     @Mapping(target = "providerDetails", source = "providerDetails", qualifiedByName = "providerDetailsDtoMap")
     ServiceOfferingDto selfDescriptionMetaToServiceOfferingDto(SelfDescriptionMeta selfDescriptionMeta,
                                                                ServiceOfferingExtension extension,
-                                                               MerlotParticipantDto providerDetails,
-                                                               String signerLegalName);
+                                                               MerlotParticipantDto providerDetails);
 }
